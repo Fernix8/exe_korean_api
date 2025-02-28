07cdb2022414671c90db4dcda6fac88381023e23
namphuong <phuongnampham7823@gmail.com>
2025-02-28 10:37:03 +0700

    fix missing payment

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { AuthModule } from './auth/auth-goole/auth.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { BlogModule } from './blog/blog.module';
 import { ConfigModule } from '@nestjs/config';
+import { PaymentModule } from './payment/payment.module';
 
 @Module({
   imports: [
@@ -15,6 +16,7 @@ import { ConfigModule } from '@nestjs/config';
     MongooseModule.forRoot(process.env.MONGODB),
     UserModule,
     AuthModule,
+    PaymentModule,
     BlogModule,
   ],
   controllers: [AppController],
