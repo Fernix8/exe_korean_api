f86926f394a4abf18467afa521a29499d0eb9bd6
CqTung137 <cquangtung1307@gmail.com>
2025-03-13 15:25:43 +0700

    New

--- src/userprofile/user.service.ts
+++ src/userprofile/user.service.ts
@@ -7,12 +7,14 @@ import { User, UserDocument } from '../schemas/user.schema';
 export class UserService {
   constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
 
-  async getUser(): Promise<User> {
-    return this.userModel.findOne();
+  async getUserById(userId: string): Promise<User> {
+    const user = await this.userModel.findById(userId);
+    if (!user) throw new NotFoundException('User not found');
+    return user;
   }
 
-  async updateUser(id: string, updateData: Partial<User>): Promise<User> {
-    const updatedUser = await this.userModel.findByIdAndUpdate(id, updateData, { new: true });
+  async updateUser(userId: string, updateData: Partial<User>): Promise<User> {
+    const updatedUser = await this.userModel.findByIdAndUpdate(userId, updateData, { new: true });
     if (!updatedUser) throw new NotFoundException('User not found');
     return updatedUser;
   }
